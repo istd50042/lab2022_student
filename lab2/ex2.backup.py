6ce1c201646c7c6675bb2b372ae4dc8594452a2a
istd50042 <105529126+istd50042@users.noreply.github.com>
2022-05-21 10:20:07 +0800

    Update ex2.backup.py

--- lab2/ex2.backup.py
+++ lab2/ex2.backup.py
@@ -1,11 +1,11 @@
-# Backup file for Lab 2 Part II
-# In case the server goes down
+# Lab 2 Part II OTP
 
 import os
 import base64
 
 
 def XOR(a, b):
+    """Encryption using XOR, do not modify"""
     r = b""
     for x, y in zip(a, b):
         r += (x ^ y).to_bytes(1, "big")
@@ -13,11 +13,12 @@ def XOR(a, b):
 
 
 def gen_OTP(length):
+    """Generate a random OTP - you are not supposed to know the key - do not modify"""
     return bytearray(os.urandom(length))
 
 
 def decrypt(cipher, OTP):
-    # DO NOT change this function
+    """Decryption also using XOR, do not modify"""
     return XOR(cipher, OTP)
 
 
@@ -31,16 +32,20 @@ OTP = gen_OTP(length=len(original_plaintext))
 original_cipher = XOR(original_plaintext, OTP)
 
 # Decrypt
+# This will print the original message
 print(decrypt(original_cipher, OTP))
 
 
 def hax():
     # TODO: manipulate ciphertext to decrypt to:
     # "Student ID 100XXXX gets 4 points"
+    # Remember your goal is to modify the encrypted message
+    # therefore, you do NOT decrypt the message here
     return new_cipher
 
 
 new_cipher = hax()
 
-# Decrypt function should provide the manipulated message:
+
+# When we finally decrypt the message, it should show the manipulated message
 print(decrypt(new_cipher, OTP))
